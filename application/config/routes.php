f5aa64a79d3911b3bd9effa195de693dabef7527
NaseebaNasi <naseebampd@gmail.com>
2016-04-06 16:43:30 +0530

    add dash bord

--- application/config/routes.php
+++ application/config/routes.php
@@ -57,32 +57,32 @@ $route['default_controller'] = 'welcome';
 
 $route['add_client']= 'Client_Controller';
 $route['dashboard/client/delete/(:any)'] ='Client_Controller/delete/$1';
- $route['dashboard/client'] = 'Client_Controller/view';
+ $route['dashboard/clients'] = 'Client_Controller/view';
 
 
 //...................admin.........
 ///category
 
-$route['add_category']= 'Category_Controller';
+$route['dashboard/add_category']= 'Category_Controller';
 $route['dashboard/category/delete/(:any)'] ='Category_Controller/delete/$1';
 $route['dashboard/category'] = 'Category_Controller/view';
 //venue
-$route['add_venue'] = 'Venue_Controller';
+$route['dashboard/add_venues'] = 'Venue_Controller';
 $route['dashboard/category'] ='Category_Controller/view_cat';
-$route['dashboard/venue'] = 'Venue_Controller/view';
+$route['dashboard/venues'] = 'Venue_Controller/view';
 $route['dashboard/venue/delete/(:any)'] ='Venue_Controller/delete/$1';
 //vehicle
 $route['dashboard/add_vehicle'] = 'Vehicle_Controller';
 $route['dashboard/vehicle/delete/(:any)'] ='Vehicle_Controller/delete/$1'; 
 $route['dashboard/vehicles'] = 'Vehicle_Controller/view';
 //entertiment
-$route['add_entertiment'] = 'Entertiment_Controller';
+$route['dashboard/add_entertiment'] = 'Entertiment_Controller';
 $route['dashboard/entertiment/delete/(:any)'] ='Entertiment_Controller/delete/$1'; 
-$route['dashboard/entrtiment'] = 'Entertiment_Controller/view';
+$route['dashboard/entertinment'] = 'Entertiment_Controller/view';
 //payment
-$route['add_payment'] = 'Payment_Controller';
+$route['dashboard/add_payment'] = 'Payment_Controller';
 $route['dashboard/payment/delete/(:any)'] ='Payment_Controller/delete/$1'; 
-$route['dashboard/payment'] = 'Payment_Controller/view';
+$route['dashboard/payments'] = 'Payment_Controller/view';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/views/admin/view_cat.php
+++ application/views/admin/view_cat.php
@@ -1,12 +1,44 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>category</title>
-	<link rel="stylesheet" href="">
+    <meta charset="UTF-8">
+    <meta name="keywords" content="web design, web development, web site development, web site design, web design development, e-commerce, ecommerce, interactive, new media, development, Manjeri, hove, Manjeri web design, Manjeri ecommerce, Manjeri e-commerce, Manjeri web development, malappuram, content management, cms, web site, web sites, psybo, psybo technologies, psybotechnologies">
+    <meta name="description" content="Psybo technologies is a small web design &amp; development agency based in Manjeri, Malappuram, INDIA. We've made a reputation for building websites that look great and are easy-to-use.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo base_url('admin/img/ico.png');?>" type="image/png" sizes="47x54">
+    <title><?php echo ucfirst($currentPage); ?> Kudumba sree</title>
+    <link rel="stylesheet" href="<?php echo base_url('admin/css/styleapp.css');?>">
+    <script type="text/javascript" src="<?php echo base_url('admin/js/appjs.js');?>"></script>
+    <style>
+        #img{
+            width: 10px;
+            height: 10px;
+        }
+    </style>
 </head>
 <body>
+<div class="page-wrapper">
+    <div class="left-wrapper">
+        <?php echo dashboard_menu('category');?>
+    </div>
+
+    <nav class="top-wrapper">
+        <div class="sidebar-top">
+            <button class="humburger pull-left">
+                <i class="fa fa-bars fa-2x center-block"></i>
+            </button>
+            <ul class="menu-top pull-right">
+                <li><a href="#"><i class="fa fa-envelope-o fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-bell-o fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-cog fa-lg"></i></a></li>
+                <li>
+                    <a href="<?php echo base_url('dashboard/logout');?>">logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+	<div>	
 	<?php if(isset($data))
 	{
 		echo $data;
